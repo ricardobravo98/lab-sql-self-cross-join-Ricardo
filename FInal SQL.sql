a1a5785a3edba5390eab8f8cb6d3391dd09d3b19
ricardobravo98 <ricardobravo98@hotmail.com>
2023-03-18 16:27:56 +0100

    Update FInal SQL.sql

--- FInal SQL.sql
+++ FInal SQL.sql
@@ -8,7 +8,17 @@ and a.film_id = b.film_id
 Join actor as c on a.actor_id = c.actor_id;
 
 -- 2
-Select customer_id, rental_id from rental a
-join film b on 
+select d.customer_id,a.customer_id, count(b.film_id) from rental a
+join inventory b on a.inventory_id = b.inventory_id
+join film c on b.film_id = c.film_id
+join rental d on a.customer_id < d.customer_id
+where b.film_id > 3
+group by d.customer_id, b.film_id;
+
+-- 3
+select a.film_id, a.actor_id,b.actor_id,b.film_id from film_actor a
+join film_actor b on a.actor_id < b.actor_id
+and a.film_id < b.film_id
+order by a.film_id, a.actor_id;
 
 
